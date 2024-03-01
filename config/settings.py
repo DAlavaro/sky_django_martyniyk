7325caec206e57e3f87733f5540fb50b2930dca6
DAlavaro <59216962+DAlavaro@users.noreply.github.com>
2024-03-01 21:46:42 +0300

    19.2

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+
+    'catalog.apps.CatalogConfig',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
